3e5bdf6186f65b0ddb540dc77799f5e26415f35a
韩曙亮 <13241153187@163.com>
2016-06-24 11:04:04 +0800

    添加 自定义崩溃界面 CustomActivityOnCrash 库 和 示例代码

--- app_sample/src/main/java/cn/org/octopus/HomeActivity.java
+++ app_sample/src/main/java/cn/org/octopus/HomeActivity.java
@@ -1,4 +1,4 @@
-package cn.org.octopus.quick;
+package cn.org.octopus;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -13,7 +13,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity
+import cn.org.octopus.quick.R;
+
+public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
---
+++ app_sample/src/main/java/cn/org/octopus/universalimageloader/UILActivity.java
@@ -0,0 +1,11 @@
+package cn.org.octopus.universalimageloader;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by octopus on 2016/6/24.
+ */
+public class UILActivity extends AppCompatActivity {
+
+
+}
